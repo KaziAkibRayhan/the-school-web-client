323fa4d335e695366a6593e7c6b2ba7b55c549bc
kazi Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-12-04 18:39:10 +0600

    display courses category and clickableroute

--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { AiOutlineArrowDown } from "react-icons/ai";
 
 const Courses = () => {
+    const [categoryCourses, setCategoryCourses] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:5000/courses-category')
+            .then(res => res.json())
+            .then(data => setCategoryCourses(data))
+    }, [])
     return (
-        <div>
-            <h3>This is our Courses</h3>
-        </div>
+        <Container>
+            <Row>
+                <Col lg='9'>Content Detalis Side</Col>
+                <Col lg='3'>
+                    <h5 className='text-primary'>Our Courses Category <AiOutlineArrowDown /></h5>
+                    <div>
+                        {
+                            categoryCourses.map(category => <p key={category.id}>
+                                <Link to={`/courses-category/${category.id}`}>
+                                    {category.name}</Link></p>)
+                        }
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
